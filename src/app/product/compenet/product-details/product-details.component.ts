f4dff266950b9784923f3ffcd6080da1c4bf7a5e
Yassine Boujnah <yassineboujnah567@gmail.com>
2026-01-03 03:07:53 +0100

    style: Update product details implementation

--- src/app/product/compenet/product-details/product-details.component.ts
+++ src/app/product/compenet/product-details/product-details.component.ts
@@ -3,16 +3,18 @@ import { ActivatedRoute, RouterLink } from '@angular/router';
 import { ProductService } from '../../services/product.service';
 import { Product } from '../../../shared/models/product';
 import { CommonModule } from '@angular/common';
+import { LoadingComponent } from "../../../shared/compenent/loading/loading.component";
 
 @Component({
   selector: 'app-product-details',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, LoadingComponent],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.css'
 })
 export class ProductDetailsComponent implements OnInit {
   product: Product = {} as Product;
+  loading: boolean = true;
   constructor(private route: ActivatedRoute, private productService: ProductService) { }
   ngOnInit(): void {
     this.getproductbyid();
@@ -20,6 +22,7 @@ export class ProductDetailsComponent implements OnInit {
   getproductbyid() {
     this.productService.getproductbyid(this.route.snapshot.params['id']).subscribe(res => {
       this.product = res;
+      this.loading = false;
     }, err => {
       console.log(err);
       alert(err.message);
